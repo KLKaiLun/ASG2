da80dbce64e8223693f0d47d014b7b51816ba3c5
KLKaiLun <kailun612@gmail.com>
2023-02-05 23:35:48 +0800

    Tried to implement API but failed (WIP)

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Intro:
 We are making a website for our clothing store called K&C to sell clothes
 
 Features of website:
-- Putting down the price for each product
+- Putting down the price and rating for each product
 
 - Categorise clothes into men, women, children, t shirts, pants, jeans etc.
 
@@ -19,3 +19,9 @@ Features of website:
 - Have a add to cart feature so people can add mutliple products and check out all at once
 
 - Allow people to choose whether to self collect or opt for delivery
+
+- Have a search feature to find items easily
+
+
+References:
+ (n.d.). Retrieved from https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/452425/item/goods_32_452425.jpg?width=750. 
--- script.js
+++ script.js
@@ -2,4 +2,44 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
     })
-    
+
+var jsondata = {"Name": "xyz","Price": "abc"};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://kclothes-1417.restdb.io/rest/product",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "63dd35e73bc6b255ed0c460e",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+
+$.ajax({
+    method: "POST",
+    url: "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/452425/item/goods_32_452425.jpg?width=750",
+    data: {Name: "Regular Collar Polo Shirt", Price: "$50"}
+})
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://kclothes-1417.restdb.io/rest/product",
+  "method": "GET",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "63dd35e73bc6b255ed0c460e",
+    "cache-control": "no-cache"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
