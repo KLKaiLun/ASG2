97c6fa25736fe9eec428265f3e17c8292cf7a006
KLKaiLun <kailun612@gmail.com>
2023-02-10 15:17:18 +0800

    Tried to add users information to database but failed

--- loginsgnup.js
+++ loginsgnup.js
@@ -27,10 +27,23 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
   });
   
+  function getUsers(limit = 10, all = true) {
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://kclothes-1417.restdb.io/rest/useraccounts",
+      "method": "GET", //we will use GET to retrieve info
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": "63dd35e73bc6b255ed0c460e",
+        "cache-control": "no-cache"
+      },
+    }
+  }
+
   $('.tab a').on('click', function (e) {
     
     e.preventDefault();
-    
     $(this).parent().addClass('active');
     $(this).parent().siblings().removeClass('active');
     
@@ -41,3 +54,41 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     $(target).fadeIn(600);
     
   });
+
+$("#submit").on("click", function (e) {
+  e.preventDefault();
+  let UserFirstName = $("#user-firstname").val();
+  let UserLastName = $("#user-lastname").val();
+  let UserEmail = $("#user-email").val();
+  let UserPassword = $("#user-password").val();
+  let jsondata = {
+    "firstname": UserFirstName,
+    "lastname": UserLastName,
+    "email": UserEmail,
+    "password": UserPassword
+  };
+
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://kclothes-1417.restdb.io/rest/useraccounts",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "63dd35e73bc6b255ed0c460e",
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(jsondata),
+    "beforeSend": function(){
+      $("#submit").prop("disabled",true);
+      $("#signup").trigger("reset");
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    $("#submit").prop("disabled",false);
+    getUsers();
+  });
+});
--- script.js
+++ script.js
@@ -3,26 +3,6 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
     })
 
-/*
-var jsondata = {"Name": "xyz","Price": "abc"};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://kclothes-1417.restdb.io/rest/product",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json",
-    "x-apikey": "63dd35e73bc6b255ed0c460e",
-    "cache-control": "no-cache"
-  },
-  "processData": false,
-  "data": JSON.stringify(jsondata)
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-*/
 var db = new restdb("63dd35e73bc6b255ed0c460e", settings);
 
 $(function () {
